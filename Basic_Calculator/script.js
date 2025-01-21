76762482d207f03bc529610bc73d4da26725032f
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-21 13:43:16 +0000

    number input started

---
+++ Basic_Calculator/script.js
@@ -0,0 +1,6 @@
+//First giving function to all buttons
+//Number buttons first
+const input = document.querySelector('#task')
+document.querySelector('#zero').addEventListener('click', (e)=>{
+    input.textContent += 0
+})
