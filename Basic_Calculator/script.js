075b0040b1696702d2aae8f8c7ea13149020e42f
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-21 17:22:43 +0000

    short changes

--- Basic_Calculator/script.js
+++ Basic_Calculator/script.js
@@ -2,5 +2,13 @@
 //Number buttons first
 const input = document.querySelector('#task')
 document.querySelector('#zero').addEventListener('click', (e)=>{
-    input.textContent += 0
+    const number = document.querySelector('.button').innerHTML
+    switch(number){
+        case 1:
+            input.textContent += 1;
+            break;
+        case 2: 
+            input.textContent += 2;
+            break;
+    }
 })
