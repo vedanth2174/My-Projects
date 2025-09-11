f3bffba79e1ca17caaebc6bb31217a658dd4a4e7
Vedant Hippalgave <vedanthstudies@gmail.com>
2025-09-11 22:24:45 +0530

    f

--- react-practice/my-app/src/components/Greeting.jsx
+++
@@ -1,4 +0,0 @@
-function Greeting (props){
-  return <h3>Hello {props.name}, Welcome back.</h3>
-}
-export default Greeting;
--- react-practice/my-app/src/components/ProfileCard.jsx
+++ react-practice/my-app/src/components/ProfileCard.jsx
@@ -1,14 +1,9 @@
-import {useState} from "react";
-
 function ProfileCard(props){
-  const [likes, setLikes] = useState(0);
   return (
     <div className="ProfileCard">
   <h1>Hii, this is {props.name}</h1>
   <p> My age is {props.age}. </p>
   <p>{props.tagline}</p>
-  <h2>Like: {likes}</h2>
-  <button onClick={()=> setLikes(likes+1)}>👍🏻</button>
   </div>
   )
 } export default ProfileCard;
